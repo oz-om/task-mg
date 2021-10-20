665d8b98ecb3689bb77ba906a166748f89a61241
oz-om <ozyoom@gmail.com>
2021-10-20 18:49:48 +0100

    another changes

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ let list = document.querySelector(".list");
 menuIcon.addEventListener('click', function(){
   list.classList.toggle('open-list')
 })
+
+
+let labls = document.querySelectorAll('label');
+
+labls.forEach(label => {
+  label.addEventListener('click', function (){
+    let chebox = document.querySelector(`#${label.getAttribute('for')}`);
+
+   
+    if (!chebox.checked) {
+      console.log("true")
+  
+      console.log(chebox)
+    } else {
+      console.log("false")
+    }
+
+  })
+})
