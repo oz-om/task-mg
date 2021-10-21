cb363e6047fbc6be11ad112b2a259e5ea52bd7de
oz-om <ozyoom@gmail.com>
2021-10-21 10:11:02 +0100

    upload changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector(".icon ion-icon ");
 let list = document.querySelector(".list");
 
-menuIcon.addEventListener('click', function(){
+menuIcon.addEventListener('click', function() {
   list.classList.toggle('open-list')
 })
 
@@ -9,17 +9,24 @@ menuIcon.addEventListener('click', function(){
 let labls = document.querySelectorAll('label');
 
 labls.forEach(label => {
-  label.addEventListener('click', function (){
+  label.addEventListener('click', function() {
     let chebox = document.querySelector(`#${label.getAttribute('for')}`);
-
-   
     if (!chebox.checked) {
       console.log("true")
-  
-      console.log(chebox)
     } else {
       console.log("false")
     }
 
   })
 })
+
+// max lengh of text is 172
+let descHint = document.querySelector('.desc-hint').textContent;
+
+if (descHint.length >= 172) {
+  let clone = document.createTextNode(descHint).cloneNode(true);
+  
+  document.querySelector('.desc-hint').textContent = descHint.substring(0, 172)+'.....';
+} else {
+  
+}
